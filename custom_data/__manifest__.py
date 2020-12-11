47d0d3128dae067c1323b5fb4bd904d665c61fed
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2020-12-11 15:19:53 +0100

    Séquences

---
+++ custom_data/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Lexis : Données Pérsonalisées',
+    'version': '1.0',
+    'category': 'Gestion Commercialle, Gestion de l\'inventaire',
+    'description': """""",
+
+    'author': 'Osisoftware',
+    'website': 'http://www.osisoftware.com',
+    'depends': ['account'],
+    'data': [
+        'data/data.xml',
+    ],
+}
