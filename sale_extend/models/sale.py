159f229dab626e91d744b98a069d7fbafc8bc8a5
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2020-12-14 11:52:44 +0100

    Révisions

--- sale_extend/models/sale.py
+++ sale_extend/models/sale.py
@@ -2,6 +2,19 @@
 
 from odoo import models, fields, api
 
+REVISIONS = [
+    (0, 'Révision A'),
+    (1, 'Révision B'),
+    (2, 'Révision C'),
+    (3, 'Révision D'),
+    (4, 'Révision E'),
+    (5, 'Révision F'),
+    (6, 'Révision G'),
+    (7, 'Révision H'),
+    (8, 'Révision I'),
+    (9, 'Révision J'),
+]
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -10,12 +23,20 @@ class SaleOrder(models.Model):
     ref_client = fields.Char('Réference Client')
     objet = fields.Char('Objet')
     texte_en_tete = fields.Text("Texte de l'en-tête")
+    revision = fields.Char('Révision', default='Révision A')
+    revision_index = fields.Integer('Révision Index', default=0)
+
+    def write(self, vals):
+        vals['revision_index'] = self.revision_index + 1
+        vals['revision'] = REVISIONS[vals['revision_index']][1]
+        res = super(SaleOrder, self).write(vals)
+        return res
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    poste = fields.Integer('Poste', compute='_compute_next_poste')
+    poste = fields.Integer('Poste', compute='_compute_next_poste', default=False)
 
     @api.depends('order_id.order_line')
     def _compute_next_poste(self):
